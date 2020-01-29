2e1f292d1c482ca0c139e694420211ef403ffee4
hmh4608 <hoang.ho@mavs.uta.edu>
2020-01-29 12:38:17 -0600

    P02 main.cpp first draft

--- P02/full_credit/color.cpp
+++ P02/full_credit/color.cpp
@@ -5,12 +5,18 @@
 //constructor initializer list
 Color::Color(int red, int green, int blue) : _red{red}, _green{green}, _blue{blue} {}
 
+/*
+method returns string representation of the RGB color
+*/
 std::string Color::to_string()
 {
 	//std::to_string(int n) turns n into a string and returns it; necessary for concatenation	
 	return '(' + std::to_string(_red) + ',' + std::to_string(_green) + ',' + std::to_string(_blue) + ')';
 }
 
+/*
+method returns parameter preceded by ANSI escape code for "set foreground color" to the _red, _green, _blue, and ends with ANSI escape code for "reset"
+*/
 std::string Color::colorize(std::string text)
 {
 	return "\033[38;2;" + std::to_string(_red) + ";" + std::to_string(_green) + ";" + std::to_string(_blue) + "m" + text + "\033[0m";
---
+++ P02/full_credit/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include "color.h"
+
+int main()
+{
+	//instantiate 3 Color objects
+	Color red{255, 0, 0};
+	Color pink{255, 167, 167};
+	Color lime{211, 255, 167};
+
+	//prints the three colors in their representative color
+	std::cout << red.colorize("Red") + " " + pink.colorize("Blush Pink") + " " + lime.colorize("Lime Green") << std::endl;
+
+	//asks user for 3 integers representing red, green, and blue
+	int r, g, b;
+
+	std::cout << "Enter the RGB values separated by a space: ";
+	std::cin >> r;
+	std::cin >> g;
+	std::cin >> b;
+	
+	//instantiate user-entered RGB color
+	Color color{r, g, b};
+
+	//prints the user-entered color's string representation in its representative color
+	std::cout << color.colorize(color.to_string()) << std::endl;
+
+	return 0;
+}
