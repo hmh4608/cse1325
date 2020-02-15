0aa90150e6c66f9d3de3d6749da8c6b703186ed9
hmh4608 <hoang.ho@mavs.uta.edu>
2020-02-15 01:13:36 -0600

    P04 full credit rev 1

--- P04/full_credit/coin.cpp
+++ P04/full_credit/coin.cpp
@@ -15,15 +15,15 @@ std::string to_string(Coin_size size)
 }
 
 //constructors
-Coin::Coin(Coin_size size, Year year) : _size{size}, _year{year}, _notes{new std::string{NULL}} {}
+Coin::Coin(Coin_size size, Year year) : _size{size}, _year{year}, _notes{nullptr} {}
 Coin::Coin() : Coin{Coin_size::PENNY, 1792} {}
-Coin::Coin(const Coin& rhs) : Coin{rhs._size, rhs._year} {}
+Coin::Coin(const Coin& rhs) : _size{rhs._size}, _year{rhs._year}, _notes{new std::string{*(rhs._notes)}} {}
 Coin::~Coin() { delete _notes; } //destructor
 
 //methods
 void Coin::add_note(std::string s)
 {
-	//checks if notes are blank
+	//checks if notes are blank/nullptr
 	if(!_notes)
 	{	
 		_notes = new std::string{s};
@@ -38,21 +38,25 @@ void Coin::add_note(std::string s)
 //copy assignment operator
 Coin& Coin::operator=(const Coin& rhs)
 {
+	if(this != &rhs)
+	{
+		_size = rhs._size;
+		_year = rhs._year;
+		_notes = new std::string{*(rhs._notes)};
+	}
+
 	return *this;
 }
 std::ostream& operator<<(std::ostream& ost, const Coin& coin)
 {
-	//year + space
-	ost << std::to_string(coin._year) + " ";
-	
-	//checking it is a type of coin or not
-	if(coin._size != Coin_size::PENNY && coin._size != Coin_size::NICKEL && coin._size != Coin_size::DIME && coin._size != Coin_size::QUARTER)
+	//checking it is a type of coin or not and whether it is a legitimate coin made after 1792
+	if(coin._size != Coin_size::PENNY && coin._size != Coin_size::NICKEL && coin._size != Coin_size::DIME && coin._size != Coin_size::QUARTER || coin._year < 1792)
 	{
 		throw std::runtime_error{"Not a coin"};
 	}
 	else
 	{
-		ost << to_string(coin._size) + "\n";
+		ost << std::to_string(coin._year) + " " + to_string(coin._size) + "\n";
 	}
 
 	if(coin._notes)
